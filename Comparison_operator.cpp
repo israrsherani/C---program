19250fe584e5e1158b5abcecad0682798aea1ada
israrsherani <israrkhansherani9@gmail.com>
2026-08-12 23:13:19 +0500

    Delete Comparison_operator.cpp

--- Comparison_operator.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main (){
-    int age = 22;
-    cout << "Adult:" <<(age >=18) << endl;
-    cout << "teenager:" << (age<18) << endl;
-    cout << "age is 22:" << (age==22) << endl;
-    return 0;
-}
