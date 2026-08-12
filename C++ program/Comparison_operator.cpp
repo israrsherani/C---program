6af8188ef2698752ce3dc05f037ea115ccd0f6ca
israrsherani <israrkhansherani9@gmail.com>
2026-08-12 23:12:13 +0500

    Add files via upload

---
+++ C++ program/Comparison_operator.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int age = 22;
+    cout << "Adult:" <<(age >=18) << endl;
+    cout << "teenager:" << (age<18) << endl;
+    cout << "age is 22:" << (age==22) << endl;
+    return 0;
+}
