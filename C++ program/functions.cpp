16778d7a7f5bd0cd46eb91373bb3cc4d03e61707
israrsherani <israrkhansherani9@gmail.com>
2026-08-23 20:08:43 +0500

    Add files via upload

---
+++ C++ program/functions.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void fun1(){
+    cout <<  "Hello Israr" << endl;
+    cout << " Welcome to C++ programming" << endl;
+}
+int main (){
+    fun1();
+    return 0;
+}
