969848e32b4c1fabc1bee065d4b78d2250599556
israrsherani <israrkhansherani9@gmail.com>
2026-08-23 20:47:24 +0500

    Add files via upload

---
+++ C++ program/func.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void roghbarh (){
+    cout << "sanga ye" << endl;
+    cout << "sha ye" << endl;
+    cout << "jor ye" << endl;
+    cout << "pakhair ye" << endl;
+    cout << "pa kor k khair dai" << endl;
+    cout << "ta pa khpala sha ye" << endl;
+    cout << "janati say" << endl;
+    cout << "bay parday masay" << endl;
+    cout << "jannnat da zai sa" << endl;
+}
+int main (){
+    roghbarh();
+    return 0;
+}
